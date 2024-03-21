05ae2ca368d3c0fe9c71460e6d2cc2c60ad8ce7d
Anuj1307 <144218708+Anuj1307@users.noreply.github.com>
2024-03-21 18:11:51 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,13 @@ recognition.onresult = function(event)
 
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
-    speak();
+    if(Content =="take my selfie")
+    {
+        console.log("taking selfie ---");
+        speak();
+    }
+
+    
 
 }
 
@@ -25,13 +31,23 @@ function speak()
 {
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").ariaValueMax;
+    speak_data = "Taking your Selfie in 5 seconds";
+
+    // speak_data = document.getElementById("textbox").value;
 
     var utterThis= new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    },5000);
 }
+
+
 
 Webcam.set({
     width:360,
@@ -40,3 +56,18 @@ Webcam.set({
     png_quality:90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
